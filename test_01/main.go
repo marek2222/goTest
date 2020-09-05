34b007cb10f1834ac007edf53f363203df759a87
marek2222 <marekmusial2@o2.pl>
2020-09-05 21:15:52 +0200

    Unit tests

--- test_01/main.go
+++ test_01/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io"
 	"net/http"
+
+	"github.com/marek2222/goTest/test_01/pakiet"
 )
 
 func main() {
-	port := "80" //os.Getenv("PORT")
+	port := ":9000" //os.Getenv("PORT")
 	fmt.Println(port)
 
 	fmt.Println("Dzień dobry")
@@ -19,11 +21,15 @@ func main() {
 }
 
 func naszHandler(w http.ResponseWriter, r *http.Request) {
-	cos := ""
-	for i := 1; i < 10; i++ {
-		cos += fmt.Sprintf("%d ", i)
-	}
-	log.Println(cos)
+	cos := pakiet.Pakiet()
+	// fmt.Println(cos)
+	io.WriteString(w, cos)
+	// cos := ""
+	// for i := 1; i < 10; i++ {
+	// 	cos += fmt.Sprintf("%d ", i)
+	// }
+	// log.Println(cos)
+
 	// fmt.Fprintln(w, "Dzień dobry.")
 	// fmt.Fprintln(w, cos)
 	// jsn, _ := json.Marshal(cos)
@@ -31,11 +37,11 @@ func naszHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// Pakiet opis
-func Pakiet() string {
-	ret := ""
-	for i := 1; i < 10; i++ {
-		ret += fmt.Sprintf("%d ", i)
-	}
-	return ret
-}
+// // Pakiet opis
+// func Pakiet() string {
+// 	ret := ""
+// 	for i := 1; i < 10; i++ {
+// 		ret += fmt.Sprintf("%d ", i)
+// 	}
+// 	return ret
+// }
