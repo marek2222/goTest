86bbdc2d44ae2e9baf989b72d8c6975b46e34469
marek2222 <marekmusial2@o2.pl>
2021-03-22 14:32:48 +0100

    echo

--- basic/books/jgopop/jgopop/r01/echo3/main.go
+++ basic/books/jgopop/jgopop/r01/echo3/main.go
@@ -1,7 +1,6 @@
 // Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
-
 // Echo3 wyświetla swoje argumenty wiersza poleceń.
 package main
 
@@ -9,11 +8,27 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 //!+
-func main() {
-	fmt.Println(strings.Join(os.Args[1:], " "))
-}
+// func main() {
+// 	fmt.Println(strings.Join(os.Args[1:], " "))
+// }
 
-//!-
+// pokazuje nazwę wywołującego go polecenia
+func main() {
+	fmt.Println(strings.Join(os.Args[0:], "; "))
+
+	fmt.Println("\nIndex: wartość")
+	for idx, arg := range os.Args {
+		fmt.Printf("    %d: %s/n", idx, arg)
+	}
+	fmt.Println()
+
+	start := time.Now()
+	fmt.Println(strings.Join(os.Args[0:], "; "))
+	fmt.Printf("Upłyneło:  %.2f sek \n", time.Since(start).Seconds())
+	fmt.Printf("Upłyneło:  %d ms\n", time.Since(start).Milliseconds())
+
+}
